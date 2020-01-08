d28310323a0cb7d20116a8e65abf50b2fe129fe5
Richard Lyon <richardlyon@fastmail.com>
2020-01-08 11:54:10 +0000

    comment out setLedColour

--- sensor-utils.h
+++ sensor-utils.h
@@ -1,14 +1,14 @@
 #pragma once
 
 // Pin assignments for a 4-wire LED for status indication
-#define RED_LED A1 //
-#define GREEN_LED A2
-#define BLUE_LED A3
+// #define RED_LED A1 //
+// #define GREEN_LED A2
+// #define BLUE_LED A3
 
-#define OFF (0)
-#define RED (1)
-#define BLUE (2)
-#define GREEN (3)
+// #define OFF (0)
+// #define RED (1)
+// #define BLUE (2)
+// #define GREEN (3)
 
 namespace utils
 {
@@ -46,33 +46,33 @@ void printBanner(const char *firmware_name,
     Serial.println();
 }
 
-void setLedColour(int colour)
-{
-    switch (colour)
-    {
-    case RED:
-        digitalWrite(RED_LED, HIGH);
-        digitalWrite(GREEN_LED, LOW);
-        digitalWrite(BLUE_LED, LOW);
-        break;
+// void setLedColour(int colour)
+// {
+//     switch (colour)
+//     {
+//     case RED:
+//         digitalWrite(RED_LED, HIGH);
+//         digitalWrite(GREEN_LED, LOW);
+//         digitalWrite(BLUE_LED, LOW);
+//         break;
 
-    case BLUE:
-        digitalWrite(RED_LED, LOW);
-        digitalWrite(GREEN_LED, LOW);
-        digitalWrite(BLUE_LED, HIGH);
-        break;
+//     case BLUE:
+//         digitalWrite(RED_LED, LOW);
+//         digitalWrite(GREEN_LED, LOW);
+//         digitalWrite(BLUE_LED, HIGH);
+//         break;
 
-    case GREEN:
-        digitalWrite(RED_LED, LOW);
-        digitalWrite(GREEN_LED, HIGH);
-        digitalWrite(BLUE_LED, LOW);
-        break;
+//     case GREEN:
+//         digitalWrite(RED_LED, LOW);
+//         digitalWrite(GREEN_LED, HIGH);
+//         digitalWrite(BLUE_LED, LOW);
+//         break;
 
-    case OFF:
-        digitalWrite(RED_LED, LOW);
-        digitalWrite(GREEN_LED, LOW);
-        digitalWrite(BLUE_LED, LOW);
-        break;
-    }
-}
+//     case OFF:
+//         digitalWrite(RED_LED, LOW);
+//         digitalWrite(GREEN_LED, LOW);
+//         digitalWrite(BLUE_LED, LOW);
+//         break;
+//     }
+// }
 }
