d4052863c022e8b8cfadfa3aa4f035e21d3cc260
Richard Lyon <richardlyon@fastmail.com>
2020-01-09 14:55:08 +0000

    simplify

--- sensor-utils.h
+++ sensor-utils.h
@@ -3,32 +3,20 @@
 namespace utils
 {
 void printBanner(const char *firmware_name,
-                 const char *firmware_version,
                  const char *firmware_slug,
-                 const char *json_protocol_version,
-                 const char *mcu,
-                 const char *firmware_os,
+                 const char *firmware_version,
                  const char *deviceID)
 {
     Serial.println();
     Serial.println("======================================================================");
     Serial.print("# ");
-    Serial.print(firmware_name);
-    Serial.print(" V");
-    Serial.println(firmware_version);
+    Serial.println(firmware_name);
     Serial.println("----------------------------------------------------------------------");
 
     Serial.print("Firmware  : ");
-    Serial.println(firmware_slug);
-
-    Serial.print("Protocol  : ");
-    Serial.println(json_protocol_version);
-
-    Serial.print("MCU       : ");
-    Serial.println(mcu);
-
-    Serial.print("OS        : ");
-    Serial.println(firmware_os);
+    Serial.print(firmware_slug);
+    Serial.print(" V");
+    Serial.println(firmware_version);
 
     Serial.print("Device ID : ");
     Serial.println(deviceID);
